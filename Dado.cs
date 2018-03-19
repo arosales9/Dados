a16be20ee2d92a8abc67ecdcfc36ce1109ec2032
arosales9 <arosales9@ucol.mx>
2018-03-18 19:01:13 -0600

    Dados

---
+++ Dado.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimulacionDados
+{
+    class Dado
+    {
+        Random r = new Random();
+
+        int[] _Dado = new int[100];
+        //int[] _Dado1 = new int[100];
+        int[] _ReptDado = new int[100];
+
+        public string Lanzar()
+        {
+            string cad = "";
+            for (int i = 0; i < 100; i++)//hasta el 99==100
+            {
+                _Dado[i]= r.Next(1, 7);
+                //_Dado1[i] = _Dado[i];
+            }
+
+            for (int i = 0; i<100; i++)
+            {
+                for (int j = 0; j<100; j++)
+                {
+                    if (_Dado[i]==_Dado[j])
+                    {
+                        _ReptDado[i]+= 1;
+                    }
+                }
+            }
+
+            for (int i = 0; i < 100; i++)
+            {
+                cad += (i+1)+" ---> El "+_Dado[i] + " se repite " + _ReptDado[i] + " veces."+Environment.NewLine;
+            }
+            return cad;
+        }
+    }
+}
---
+++ Form1.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SimulacionDados
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnLanzar_Click(object sender, EventArgs e)
+        {
+            txtRespuesta.Clear();
+            Dado d = new Dado();
+            txtRespuesta.Text=d.Lanzar().ToString();
+        }
+    }
+}
